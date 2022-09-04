27c4927682b2d349ea64878a888afcd49829ce8e
Craig Thacker <craigthackerx@gmail.com>
2022-09-04 03:32:15 +0100

    update checker

--- terraform/environment.tf
+++ terraform/environment.tf
@@ -1,12 +1,15 @@
 variable "short" {
   description = "This is passed as an environment variable, it is for a shorthand name for the environment, for example hello-world = hw"
   type        = string
+  default     = "ldo"
 }
 
 variable "env" {
   description = "This is passed as an environment variable, it is for the shorthand environment tag for resource.  For example, production = prod"
   type        = string
+  default     = "dev"
 }
+
 variable "loc" {
   description = "The shorthand name of the Azure location, for example, for UK South, use uks.  For UK West, use ukw. Normally passed as TF_VAR in pipeline"
   type        = string
