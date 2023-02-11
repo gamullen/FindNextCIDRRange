42408c43dae348da4a662064b6de102e03881975
Craig Thacker <craigthackerx@gmail.com>
2023-02-11 01:48:03 +0000

    update code

--- src/Python/Find-NextCidrRange/__init__.py
+++ src/Python/Find-NextCidrRange/__init__.py
@@ -1,41 +1,23 @@
-from azure.identity import ClientSecretCredential
+from azure.identity import DefaultAzureCredential
 from azure.mgmt.network import NetworkManagementClient
 from ipaddress import IPv4Network
-import os
+import azure.functions as func
 import logging
-
-# Set logging level. .DEBUG, .WARNING, .INFO etc
-logging.basicConfig(level=logging.WARNING)
-
-# create a stream handler and set its level to logging.DEBUG
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
-
-# add the stream handler to the root logger
-root_logger = logging.getLogger()
-root_logger.addHandler(console_handler)
-
-sub_id = os.environ["ARM_SUBSCRIPTION_ID"]
-rg_name = "rg-ldo-euw-dev-build"
-vnet_name = "vnet-ldo-euw-dev-01"
+import datetime
 
 
-def azure_authenticate():
-    try:
-        client_id = os.environ["ARM_CLIENT_ID"]
-        client_secret = os.environ["ARM_CLIENT_SECRET"]
-        tenant_id = os.environ["ARM_TENANT_ID"]
-        credentials = ClientSecretCredential(
-            client_id=client_id, client_secret=client_secret, tenant_id=tenant_id
-        )
-        return credentials
-    except Exception as e:
-        logging.error(f"Error encountered when trying to authenticate to Azure: {e}")
+def main(findnextcidr: func.HttpRequest) -> func.HttpResponse:
+    utc_timestamp = (
+        datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    )
+
+    credential = DefaultAzureCredential()
 
 
 def find_available_subnet(
     subscription_id, resource_group_name, virtual_network_name, new_subnet_size
 ):
+    credential = DefaultAzureCredential()
 
     try:
         # Checks that new subnet size is integer, and it is compliant with RFC standard
@@ -43,7 +25,7 @@ def find_available_subnet(
         if 0 <= new_subnet_size <= 32:
             # Create a Network Management Client
             network_client = NetworkManagementClient(
-                azure_authenticate(), subscription_id=subscription_id
+                credential, subscription_id=subscription_id
             )
 
             list_virtual_networks = network_client.virtual_networks.list(
@@ -57,10 +39,12 @@ def find_available_subnet(
                     break
 
             if virtual_network is None:
-                logging.error(
+                return func.HttpResponse(
                     "Virtual network {virtual_network_name} not found in resource group "
-                    "{resource_group_name}"
+                    "{resource_group_name}",
+                    status_code=400,
                 )
+
             else:
                 # Check if the specified CIDR is within the range of the address prefixes of the virtual network
                 address_prefixes = virtual_network.address_space.address_prefixes
@@ -77,11 +61,12 @@ def find_available_subnet(
                         break
 
                 if not cidr_within_range:
-                    logging.error(
+
+                    return func.HttpResponse(
                         f"VNet {resource_group_name}/{virtual_network_name} cannot accept a "
-                        f"subnet of size {new_subnet_size}"
+                        f"subnet of size {new_subnet_size}",
+                        status_code=400
                     )
-                    exit(1)
                 else:
                     # Get the subnets of the virtual network object return from Azure
                     used_subnets = [
@@ -96,19 +81,22 @@ def find_available_subnet(
                         if not any(
                             subnet.subnet_of(used) for used in used_subnets
                         ) and not any(subnet.overlaps(used) for used in used_subnets):
-                            return subnet.__str__()
-                    return None
+                            return func.HttpResponse(
+                                subnet.__str__(),
+                                status_code=200
+                            )
+                    return func.HttpResponse(
+                        status_code=404
+                    )
         else:
             logging.error("Invalid CIDR size, must be between 0 and 32")
+            return func.HttpResponse(
+                "Invalid CIDR size, must be between 0 and 32",
+                status_code=400
+            )
     except Exception as e:
         logging.error(f"Error has been encountered {e}")
-
-
-result = find_available_subnet(
-    subscription_id=sub_id,
-    resource_group_name=rg_name,
-    virtual_network_name=vnet_name,
-    new_subnet_size="32",
-)
-
-print(result)
+        return func.HttpResponse(
+            f"Error has been encountered {e}",
+            status_code=400
+        )
